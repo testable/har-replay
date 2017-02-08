3e16a7d52348c0f52755003d8bcac9ae40bbc555
Avi Stramer <astramer@gmail.com>
2017-02-07 23:17:10 -0600

    injectable request module

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-
 const fs = require('fs');
 const moment = require('moment');
 const request = require('request');
-const url = require('url');
 const _ = require('lodash');
 
 function load(file, options) {
     options = options || {};
     options.timeout = _.isNumber(options.timeout) ? options.timeout : 60000;
+    const requestModule = options.request || request;
 
     fs.readFile(file, function(err, contents) {
         if (err) 
             throw err;
 
-        var har = JSON.parse(contents);
+        const har = JSON.parse(contents);
 
         if (_.isUndefined(har) || 
             _.isUndefined(har.log) || 
@@ -21,21 +20,21 @@ function load(file, options) {
             har.log.entries.length === 0)
             return;
 
-        var onFinish = _.after(har.log.entries.length, function() {
+        const onFinish = _.after(har.log.entries.length, function() {
             if (options.onFinish)
                 options.onFinish();
         });
 
-        var firstTime = moment(har.log.entries[0].startedDateTime);
+        const firstTime = moment(har.log.entries[0].startedDateTime);
         _.forEach(har.log.entries, function(entry) {
             if (options.beforeRequest)
                 options.beforeRequest(entry.request);
-            var delay = moment(entry.startedDateTime).diff(firstTime, 'miliseconds');
+            const delay = moment(entry.startedDateTime).diff(firstTime, 'miliseconds');
 
             if (entry.request.postData && _.isUndefined(entry.request.postData.params))
                 entry.request.postData.params = [];
             _.delay(function() {
-                var req = request({ har: entry.request, timeout: options.timeout }, function(error, response, body) {
+                const req = requestModule({ har: entry.request, timeout: options.timeout }, function(error, response, body) {
                     if(response && options.onResponse)
                         options.onResponse(response, entry.request);
                 });
