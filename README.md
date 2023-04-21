2131c46171adc990f429584fb704529cb11c05ab
avistramer <astramer@gmail.com>
2023-04-21 12:30:12 -0500

    switch over to axios

--- README.md
+++ README.md
@@ -26,8 +26,8 @@ Load and replay the request in the HAR file found at local path `file`. A delay 
 **options**
 
 * `timeout`: A timeout (milliseconds) to wait for a reply. Defaults to 1 minute.
-* `beforeRequest`: A `function(request) { ... }` to manipulate the request before it is replayed. If this method returns `false` then the request will not be replayed.
-* `onResponse`: A `function(response, request, body) { ... }` to process the response that results from a replayed request. `body` refers to the response body.
+* `beforeRequest`: A `function(request) { ... }` to manipulate the request before it is replayed. If this method returns `false` then the request will not be replayed. If your function returns a promise, the library will `await` it before continuing on to make the request.
+* `onResponse`: A `function(response, request) { ... }` to process the response that results from a replayed request. See  for the axios response schema.
 * `onFinish`: A callback function that gets called once all requests have been replayed and all responses (or errors) have been received.
 * `onError`: A `function(error, request) { ... }` callback when any error occurs during replay.
 * `replayCachedEntries`: Whether or not to replay requests where the response was loaded from the browser cache. By default this is `false`.
@@ -35,13 +35,13 @@ Load and replay the request in the HAR file found at local path `file`. A delay 
 Full Example:
 
 ```javscript
-var harReplay = require('har-replay');
+const harReplay = require('har-replay');
 harReplay.load('example.har', { 
 	beforeRequest: function(request) {
 		request.headers['Some-New-Header'] = 'abc';
 	},
 	onResponse: function(response, request, body) {
-		console.log('Response status code for ' + request.url + ': ' + response.statusCode);
+		console.log(`Response status code for ${request.url}: ${response.status}`);
 	},
 	onFinish: function() {
 		console.log('All done!');
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const moment = require('moment');
-const request = require('request');
 const _ = require('lodash');
+const { default: axios } = require('axios');
 
 function isCachedRequest(entry) {
     var response = entry.response;
@@ -9,65 +9,85 @@ function isCachedRequest(entry) {
     return (response.status == 304 || (resBodySize === 0 && response.content && response.content.size > 0));
 };
 
-function load(file, options) {
+async function load(file, options) {
     options = options || {};
     options.timeout = _.isNumber(options.timeout) ? options.timeout : 60000;
     options.replayCachedEntries = _.isBoolean(options.replayCachedEntries) ? options.replayCachedEntries : false;
 
-    const requestModule = options.request || request;
+    const axiosModule = options.axios || axios;
 
-    fs.readFile(file, function(err, contents) {
-        if (err) 
-            throw err;
+    const contents = await fs.readFile(file);
+    const har = JSON.parse(contents);
 
-        const har = JSON.parse(contents);
+    if (_.isUndefined(har) || 
+        _.isUndefined(har.log) || 
+        _.isUndefined(har.log.entries) ||
+        har.log.entries.length === 0) {
+        if (options.onFinish)
+            options.onFinish();
+        return;
+    }
 
-        if (_.isUndefined(har) || 
-            _.isUndefined(har.log) || 
-            _.isUndefined(har.log.entries) ||
-            har.log.entries.length === 0)
-            return;
+    const onFinish = _.after(har.log.entries.length, function() {
+        if (options.onFinish)
+            options.onFinish();
+    });
+    const firstTime = moment(har.log.entries[0].startedDateTime);
+    _.forEach(har.log.entries, (entry) => {
+        if (options.replayCachedEntries || !isCachedRequest(entry)) {
+            const delay = moment(entry.startedDateTime).diff(firstTime, 'miliseconds');
+            const request = entry.request;
 
-        const onFinish = _.after(har.log.entries.length, function() {
-            if (options.onFinish)
-                options.onFinish();
-        });
-
-        const firstTime = moment(har.log.entries[0].startedDateTime);
-        _.forEach(har.log.entries, function(entry) {
-            if (options.replayCachedEntries || !isCachedRequest(entry)) {
-                const delay = moment(entry.startedDateTime).diff(firstTime, 'miliseconds');
-
-                if (entry.request.postData && _.isUndefined(entry.request.postData.params))
-                    entry.request.postData.params = [];
-                _.delay(function() {
-                    const headers = [];
-                    _.forEach(entry.request.headers, function(header) {
+            if (request.postData && _.isUndefined(request.postData.params))
+                request.postData.params = [];
+            _.delay(async () => {
+                try {
+                    const headers = {};
+                    let data;
+                    _.forEach(request.headers, function(header) {
                         if (header.name.indexOf(':') !== 0)
-                            headers.push(header);
+                            headers[header.name] = header.value;
                     });
-                    entry.request.headers = headers;
                     if (options.beforeRequest) {
-                        const cont = options.beforeRequest(entry.request);
+                        let cont = options.beforeRequest(request);
+                        if (cont && cont.then)
+                            cont = await cont;
                         if (_.isBoolean(cont) && !cont) {
                             onFinish();
                             return;
                         }
                     }
-                    requestModule({ har: entry.request, timeout: options.timeout }, function(error, response, body) {
-                        if (error !== null) {
-                            if (options.onError)
-                                options.onError(error, entry.request);
-                        } else if(response && options.onResponse) {
-                            options.onResponse(response, entry.request, body);
-                        }
-                        onFinish();
+
+                    if (request.postData) {
+                        const form = new FormData();
+                        _.forEach(request.postData.params, (p) => {
+                            if (p.fileName) {
+                                form.append(p.name, p.value ? Buffer.from(p.value) : fs.createReadStream(p.fileName), p.fileName);
+                            } else if (p.value) {
+                                form.append(p.name, p.value);
+                            }
+                        });
+                        data = form;
+                    }
+
+                    const response = await axiosModule.request({
+                        method: request.method,
+                        url: request.url,
+                        headers,
+                        data,
+                        timeout: options.timeeout
                     });
-                }, delay);
-            } else {
+                    if(response && options.onResponse)
+                        options.onResponse(response, request);
+                } catch (err) {
+                    if (options.onError)
+                        options.onError(err, entry.request);
+                }
                 onFinish();
-            }
-        });
+            }, delay);
+        } else {
+            onFinish();
+        }
     });
 }
 
